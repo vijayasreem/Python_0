65f9d80e41f34abd7a6c246e1cd4397de9d49546
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-06 16:57:33 +0530

    initial commit

--- app/app.py
+++ app/app.py
@@ -1,33 +1,84 @@
-@app.route('/payment', methods=['GET','POST'])
-def payment():
-    payment_methods = ['credit/debit cards', 'PayPal', 'Stripe']
-    if request.method == 'POST':
-        selected_payment_method = request.form.get('payment_method')
-        if selected_payment_method not in payment_methods:
-            return redirect(url_for('error'))
-        else:
-            return redirect(url_for('secure_payment_page'))
-    return render_template('payment.html', payment_methods=payment_methods)
+# Flask API for Email Notifications
 
-@app.route('/secure_payment_page', methods=['GET','POST'])
-def secure_payment_page():
-    if request.method == 'POST':
-        payment_data = request.form
-        payment_status = process_payment(payment_data)
-        if payment_status:
-            return redirect(url_for('payment_success'))
-        else:
-            return redirect(url_for('payment_failed'))
-    return render_template('secure_payment_page.html')
+from flask import Flask, request, jsonify
+from flask_mail import Mail, Message
 
-@app.route('/payment_success')
-def payment_success():
-    return render_template('payment_success.html')
+# Create Flask App
+app = Flask(__name__)
 
-@app.route('/payment_failed')
-def payment_failed():
-    return render_template('payment_failed.html')
+# Setup Mail Configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'example@gmail.com'
+app.config['MAIL_PASSWORD'] = 'example_password'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
-@app.route('/error')
-def error():
-    return render_template('error.html')
+mail = Mail(app)
+
+# Create Welcome Email Route
+@app.route('/email/welcome', methods=['POST'])
+def send_welcome_email():
+    data = request.get_json()
+    user_name = data['user_name']
+    user_email = data['user_email']
+    msg = Message('Welcome to our Website', sender='example@gmail.com', recipients=[user_email])
+    msg.body = f"""
+    Dear {user_name},
+    Welcome to our website. We hope you have an enjoyable experience using our services.
+    Thank you.
+    """
+    mail.send(msg)
+    return jsonify({"message": "Welcome email sent successfully"})
+
+# Create Password Reset Email Route
+@app.route('/email/reset_password', methods=['POST'])
+def send_reset_password_email():
+    data = request.get_json()
+    user_name = data['user_name']
+    user_email = data['user_email']
+    reset_link = data['reset_link']
+    msg = Message('Reset Password Request', sender='example@gmail.com', recipients=[user_email])
+    msg.body = f"""
+    Dear {user_name},
+    We received a request to reset your password. Please click on the link below to reset your password.
+    {reset_link}
+    Thank you.
+    """
+    mail.send(msg)
+    return jsonify({"message": "Password reset email sent successfully"})
+
+# Create Order Confirmation Email Route
+@app.route('/email/order_confirmation', methods=['POST'])
+def send_order_confirmation_email():
+    data = request.get_json()
+    user_name = data['user_name']
+    user_email = data['user_email']
+    order_details = data['order_details']
+    msg = Message('Order Confirmation', sender='example@gmail.com', recipients=[user_email])
+    msg.body = f"""
+    Dear {user_name},
+    Your order has been confirmed. Below are the details of your order:
+    {order_details}
+    Thank you.
+    """
+    mail.send(msg)
+    return jsonify({"message": "Order confirmation email sent successfully"})
+
+# Create Account Activity Alert Email Route
+@app.route('/email/account_activity_alert', methods=['POST'])
+def send_account_activity_alert_email():
+    data = request.get_json()
+    user_name = data['user_name']
+    user_email = data['user_email']
+    alert_details = data['alert_details']
+    msg = Message('Account Activity Alert', sender='example@gmail.com', recipients=[user_email])
+    msg.body = f"""
+    Dear {user_name},
+    We have detected unusual activity in your account. Below are the details of the activity:
+    {alert_details}
+    Please take appropriate action to secure your account.
+    Thank you.
+    """
+    mail.send(msg)
+    return jsonify({"message": "Account activity alert email sent successfully"})
